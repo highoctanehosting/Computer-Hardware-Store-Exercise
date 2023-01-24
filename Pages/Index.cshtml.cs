2d9a8a16d88facb7b3ad39c614c364e8897486f5
High Octane Hosting <73814322+highoctanehosting@users.noreply.github.com>
2023-01-24 18:44:25 -0500

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using ComputerHardwareStore.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+
+namespace ComputerHardwareStore.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Models.Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
